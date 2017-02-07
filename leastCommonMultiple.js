402b31cdfe004426be3c05a23314cd4182ea0fac
James Attard <attaj002@mfsa.internal>
2017-02-07 18:16:21 +0100

    LCM

--- leastCommonMultiple.js
+++ leastCommonMultiple.js
@@ -1,17 +1,12 @@
-// lcm(a, b) = ab/gcd(a, b)
-
 function smallestCommons(arr) {
 
-  function lcm(a,b) {
-    for (var i=a; i<=b; i++){
-      return lcm()
-    }
-    return (a*b)/gcd(a,b);
-  }
+  // Variable declarations
+  var a, b;
+  var lcmResult;
+  var fullArr = [];
 
   function gcd(a,b) { // Euclidean algorithm recursive function
     if (b === 0) {
-      //console.log(a);
       return a;
     }
     return gcd(b, a%b);
@@ -24,22 +19,36 @@ function smallestCommons(arr) {
       b = a%b;
       a = temp;
     }
-    //console.log(a);
     return a;
   }
 
-  gcd2(10, 15);
-  return arr;
+  for (var i=arr.sort()[0]; i<=arr.sort()[arr.length-1]; i++) {
+    fullArr.push(i);
+  }
+  fullArr = fullArr.reverse();
+
+  // lcm(a, b) = ab/gcd(a,b)
+  for (var i=0; i<=fullArr.length-2; i++){
+    a = lcmResult;
+    if (i == 0) {
+      a = fullArr[i];
+    }
+    b = fullArr[i+1];
+    lcmResult = (a*b)/gcd(a,b);
+  }
+
+  console.log(lcmResult);
+  return lcmResult;
 }
 
-smallestCommons([1, 5])
+smallestCommons([1, 5]);
 // should return 60.
 
-// smallestCommons([5, 1])
-// // should return 60.
-//
-// smallestCommons([1, 13])
-// // should return 360360.
-//
-// smallestCommons([23, 18])
-// // should return 6056820.
+smallestCommons([5, 1])
+// should return 60.
+
+smallestCommons([1, 13])
+// should return 360360.
+
+smallestCommons([23, 18])
+// should return 6056820.
